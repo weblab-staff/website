d8ca2fe83a320b4ad02eafec99fc3e4ba274d01e
Cory Lynch <cjl2625@gmail.com>
2018-10-09 19:05:28 -0400

    Split out staff into team.json

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
 const main = require('./config.json');
 const winners = require('./winners.json');
 const sponsors = require('./sponsors.json');
+const team = require('./team.json');
 
 var exports = {
     ...main,
     winners: {...winners},
     sponsors: {...sponsors},
+    team: {...team},
 }
 
 module.exports = exports;
