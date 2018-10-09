f7e8ff04cc1c861127925783a25789fc0edf58a3
Cory Lynch <cjl2625@gmail.com>
2018-10-09 18:59:26 -0400

    Move sponsors into sponsors.json.

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 const main = require('./config.json');
 const winners = require('./winners.json');
+const sponsors = require('./sponsors.json');
 
 var exports = {
     ...main,
     winners: {...winners},
+    sponsors: {...sponsors},
 }
 
 module.exports = exports;
