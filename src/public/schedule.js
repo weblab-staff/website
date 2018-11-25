8212f90326c825afc5adca7cfda141314ae09150
Aaron Sipser <ajsipser@gmail.com>
2018-11-25 03:57:49 -0500

    schedule auto-scroll corrected to january only

--- src/public/schedule.js
+++ src/public/schedule.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const number_arr = ['one','two','three','four'];
-    const date = Math.floor(new Date().getDate() / 7);
-    window.scrollTo(0,document.getElementById(`week${number_arr[date]}`).offsetTop);
+    if (new Date().getMonth() == 0) { //scroll to correct week if it's january
+        const number_arr = ['one','two','three','four'];
+        const date = Math.floor(new Date().getDate() / 7);
+        window.scrollTo(0,document.getElementById(`week${number_arr[date]}`).offsetTop);
+    }
 }); 
