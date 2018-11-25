5550283f56903c3107b6818881673f2ddccd400d
Aaron Sipser <ajsipser@gmail.com>
2018-11-25 04:24:01 -0500

    removed dead carosol code

--- src/public/script.js
+++ src/public/script.js
@@ -1,35 +1,5 @@
 const FILTERS = new Set(["block", "date", "lec", "oh", "event", "milestone"]);
 
-function cycleFeatures() {
-  if (window.location.pathname === "/") {
-    var children = document.getElementById("carasol").childNodes;
-    var len = children.length;
-    var i = 0;
-
-    children[i].classList.add("show");
-
-    if (len > 1) {
-      setInterval(function() {
-        if (++i >= len) {
-          i = 0;
-        }
-        hideFeatures();
-        children[i].classList.add("show");
-      }, 5000);
-    }
-  }
-}
-
-function hideFeatures() {
-  var children = Array.prototype.slice.call(
-    document.getElementById("carasol").childNodes
-  );
-
-  for (child of children) {
-    child.classList.remove("show");
-  }
-}
-
 function showDetail(id) {
   document.getElementById(id).classList.toggle("show");
 }
@@ -103,7 +73,6 @@ console.log("Hello, World!");
 // When scrolled, make background of header
 window.onload = function() {
   updateNavSolid();
-  cycleFeatures();
 };
 
 window.onscroll = updateNavSolid;
