bead7a780b851084bea60c3b3db3ad99c151867a
Aaron Sipser <ajsipser@gmail.com>
2019-01-07 20:40:13 -0500

    fixed schedule scrolling issues

--- src/public/schedule.js
+++ src/public/schedule.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     if (new Date().getMonth() == 0) { //scroll to correct week if it's january
         const number_arr = ['one','two','three','four'];
-        const date = Math.floor(new Date().getDate() / 7);
-        window.scrollTo(0,document.getElementById(`week${number_arr[date]}`).offsetTop);
+        const date = Math.min(Math.floor(new Date().getDate() / 7)-1,0);
+        window.scrollTo(0,document.getElementById(`week${number_arr[date]}`).offsetTop - 100);
     }
 }); 
